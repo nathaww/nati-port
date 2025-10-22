9feb9d19c1978aac349c632e581e8047434e8b09
Nathan <solomonnathan065@gmail.com>
2025-10-22 21:29:04 +0300

    iytdkythg,

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="bg-red-900">
+    <html lang="en" className="bg-black">
       <body className="antialiased p-0 m-0 w-full h-full">
         <SmoothScrolling>
           <PreloaderWrapper>
